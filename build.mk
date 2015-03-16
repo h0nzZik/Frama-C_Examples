3d033e85b14a5501e874d3de08d90d4fb9a25b4a
Jan Tusil <410062@mail.muni.cz>
2015-03-16 09:43:14 +0100

    Make: 'wp' target

--- build.mk
+++ build.mk
@@ -6,13 +6,14 @@ FRAMA_C_SHARE	= $(shell frama-c -print-share-path)
 ALL_INCLUDES	= ${INCLUDES} -I${FRAMA_C_SHARE}/libc
 ALL_SOURCES		=  ${FRAMA_C_SHARE}/libc.c ${SOURCES}
 # C Preprocessor
+# option -C does not discard comments
+
 CPP			= gcc -C -E -nostdinc ${ALL_INCLUDES} -o %2 %1
 
 
-# option -C does not discard comments
 frama_project: ${ALL_SOURCES}
 	frama-c -debug 0 -pp-annot -cpp-command '${CPP}' ${ALL_SOURCES} -rte -save frama_project
 
-frama:
+wp:
 	rm -rf ./proofs
-	frama-c -load frama-project -wp-rte ${TARGETS} -wp-out ./proofs
+	frama-c -load frama_project -wp-rte -wp-fct ${TARGETS} -wp-out ./proofs
